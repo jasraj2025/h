fd44ab0aa839b8f293040d1825140fac94264475
jasraj2025 <80264078+jasraj2025@users.noreply.github.com>
2021-11-08 19:13:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,117 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(640,640);
+    canvas.position(300,200);
+    video = createCapture(VIDEO);
+    video.hide();
+
+}
+
+function draw(){
+    image(video,0,0,640,480);
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(650,50,70);
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(650,100,70)
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(650,150,70)
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(650,200,70)
+
+
+
+
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(1,50,70);
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(1,100,70)
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(1,150,70)
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(1,200,70)
+
+    
+
+
+
+
+
+
+    
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(650,250,70);
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(650,300,70)
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(650,350,70)
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(650,400,70)
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(650,450,70)
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+        fill(0,128,0);
+    stroke(0,128,0);
+    circle(1,250,70);
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(1,300,70)
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(1,350,70)
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(1,400,70)
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    circle(1,450,70)
+}
+
